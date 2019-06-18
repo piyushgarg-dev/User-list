0018cdeb1510890d319e9c472e78057b34a93b3a
Piyush Garg <gargpiyush195@gmail.com>
2019-06-18 14:58:45 +0530

    Add files via upload

--- user.js
+++ user.js
@@ -11,6 +11,7 @@ window.addEventListener('load', ()=>{
         <td>${user.email}</td>
         <td>${user.gender}</td>
         <td>${user.address}</td>
+        <td><button onClick="dltItem(this)" class="btn btn-danger btn-sm">X</button></td>
     </tr>
         `;
 
@@ -23,3 +24,23 @@ window.addEventListener('load', ()=>{
 document.getElementById('refresh').addEventListener('click',()=>{
     window.location.reload();
 });
+
+function dltItem(el)
+{
+   var str =  el.parentElement.parentElement.innerHTML.split('<td>');
+   var len = str[2].split('</td>');
+    var emailId = len[0];
+
+    var storageData = JSON.parse(window.localStorage.getItem('users'));
+    storageData.forEach((data,index)=>{
+        if(data.email == emailId)
+        {
+            storageData.splice(index,1);
+            
+        }
+    });
+    console.log(storageData);
+    window.localStorage.setItem('users',JSON.stringify(storageData));
+    window.location.reload();
+
+}
